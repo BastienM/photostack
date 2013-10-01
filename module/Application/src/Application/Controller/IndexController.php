764f718e70d498b5f953cb8393997750ce3708c2
Bastien <bastien.mennesson.pro@gmail.com>
2013-10-01 14:57:05 +0200

    Setting up styling, begun random_user_selection()

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -10,12 +10,52 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Db\TableGateway\TableGateway;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        /**
+         * $db is a SQL adapter to connect database throught
+         * PHP's PDO object
+         *
+         * @var object
+         */
+        $db = new \Zend\Db\Adapter\Adapter(array(
+            'driver' => 'Pdo_Mysql',
+            'database' => 'zend_gallery',
+            'username' => 'root',
+            'password' => 'debian'
+            ));
+
+        /*
+         * Choosing a random number between 1 and the total of users
+         * then retrieving his associated username
+         */
+        // $sql = "SELECT COUNT(pseudo) FROM users";
+
+        $username = "Hellow";
+
+        /**
+         * $imagesTable is a clone of a table
+         * with the same name in Database
+         *
+         * @var array
+         * @param   string $table name of the table to clone
+         * @param   object $db Db Adapter to use to connect
+         */
+        $imagesTable = new TableGateway('images', $db);
+
+        /*
+         * Picking up only the photos whose are owned by the selected user
+         */
+        $imageSet = $imagesTable->select(array('owner' => $username));
+
+        return new ViewModel(array(
+            'images' => $imageSet,
+            'user'   => $username,
+            ));
     }
 }
